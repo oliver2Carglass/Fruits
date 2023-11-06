ff464f5638c0230c6a8f53d8529ad939ca1cc76d
Olivier2Carglass <104084572+oliver2Carglass@users.noreply.github.com>
2023-11-05 21:44:51 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//afficher les elements de Bourne.JSON dans la page HTML dans un tableau avec une boucle for en utilisant fetch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var outputElement = document.getElementById("output");
+//
+//// Générez du contenu HTML en utilisant les données JSON
+//var htmlContent = "<ul>";
+//for (var key in jsonData) {
+//    if (jsonData.hasOwnProperty(key)) {
+//        htmlContent += "<li><strong>" + key + ":</strong> " + jsonData[key] + "</li>";
+//    }
+//}
+//htmlContent += "</ul>";
+//
+//// Affichez le contenu généré dans la page HTML
+//outputElement.innerHTML = htmlContent;
